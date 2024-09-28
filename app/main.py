0a8fe8dce14800724594822e986897ebc11c728f
pranay-agarwal01 <pranay@everstage.com>
2024-09-28 16:15:48 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -8,8 +8,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, addr = server_socket.accept() # wait for client
-    client.send(b"+PONG\r\n")
+    conn, addr = server_socket.accept() # wait for client
+    
+    with conn:
+        while True:
+            conn.recv(1024)
+            conn.send("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":

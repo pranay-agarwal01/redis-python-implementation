9d49d416439baec71eae9da6097aa41d8585e1cc
pranay-agarwal01 <pranay@everstage.com>
2024-09-28 17:45:38 +0530

    stage 5

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 import socket  # noqa: F401
 import threading
 
-def on_new_client(conn,addr):
-    while True:
-        msg = conn.recv(1024)
-        #do some checks and if msg == someWeirdSignal: break:
-        print("Received: ", addr, " >> ", msg)
-        #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-        conn.send("+PONG\r\n".encode())
-
+def connect(connection: socket.socket) -> None:
+    with connection:
+        connected: bool = True
+        while connected:
+            command:str = connection.recv(1024).decode()
+            print(f"DEBUG: recieved - {command.encode()}")
+            connected = bool(command)
+            response: str  = "+PONG\r\n"
+            if "ECHO" in command:
+                res_data = command.split("\r\n")[-2]
+                content_len = len(res_data)
+                response = f"${content_len}\r\n{res_data}\r\n"
+                
+            print(f"DEBUG: returning - {response.encode()}")
+            connection.send(response.encode())
 
 def main():
     print("Running redis server on port 6379")
@@ -17,9 +24,8 @@ def main():
 
     while True:
         conn, addr = server_socket.accept() # wait for client
-        thread = threading.Thread(target=on_new_client, args=(conn,addr))
+        thread = threading.Thread(target=connect, args=[conn])
         thread.start()
-
 
 if __name__ == "__main__":
     main()

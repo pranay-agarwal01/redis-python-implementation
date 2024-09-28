2f72e1b9f959af539c0bcbc74b153b613bc69b63
pranay-agarwal01 <pranay@everstage.com>
2024-09-28 16:29:44 +0530

    stage 4

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
 import socket  # noqa: F401
+import threading
+
+def on_new_client(conn,addr):
+    while True:
+        msg = conn.recv(1024)
+        #do some checks and if msg == someWeirdSignal: break:
+        print("Received: ", addr, " >> ", msg)
+        #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
+        conn.send("+PONG\r\n".encode())
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    print("Running redis server on port 6379")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    
-    with conn:
-        while True:
-            conn.recv(1024)
-            conn.send("+PONG\r\n".encode())
+
+    while True:
+        conn, addr = server_socket.accept() # wait for client
+        thread = threading.Thread(target=on_new_client, args=(conn,addr))
+        thread.start()
 
 
 if __name__ == "__main__":

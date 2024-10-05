4d33e47b761ce9537b8ee1f3c41d640b8e541345
pranay-agarwal01 <pranay@everstage.com>
2024-10-05 18:05:55 +0530

    set and get

--- app/main.py
+++ app/main.py
@@ -1,5 +1,30 @@
-import socket  # noqa: F401
+import socket   
 import threading
+
+db_data = {}
+
+def command_parser(command: str) -> str:
+    all_tokens = command.split("\r\n")
+    #Sample Commands
+    # *1\r\n$4\r\nPING\r\n
+    # *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n 
+    # *3\r\n$3\r\nSET\r\n$3\r\nhey\r\n$3\r\nyou\r\n
+    # *2\r\n$3\r\nGET\r\n$3\r\nhey\r\n
+    print("DEBUG: Command Tokens", all_tokens)
+    if all_tokens[0] == "*1" and all_tokens[1] == "$4" and all_tokens[2] == "PING":
+        return "+PONG\r\n"
+    elif all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "ECHO":
+        return f"${len(all_tokens[4])}\r\n{all_tokens[4]}\r\n"
+    elif all_tokens[0] == "*3" and all_tokens[1] == "$3" and all_tokens[2] == "SET":
+        db_data[all_tokens[4]] = all_tokens[6]
+        return "+OK\r\n"
+    elif all_tokens[0] == "*2" and all_tokens[1] == "$3" and all_tokens[2] == "GET":
+        key = all_tokens[4]
+        if key not in db_data:
+            return "$-1\r\n"
+        return f"${len(db_data[key])}\r\n{db_data[key]}\r\n"
+    else:
+        return "$-1\r\n"
 
 def connect(connection: socket.socket) -> None:
     with connection:
@@ -8,11 +33,7 @@ def connect(connection: socket.socket) -> None:
             command:str = connection.recv(1024).decode()
             print(f"DEBUG: recieved - {command.encode()}")
             connected = bool(command)
-            response: str  = "+PONG\r\n"
-            if "ECHO" in command:
-                res_data = command.split("\r\n")[-2]
-                content_len = len(res_data)
-                response = f"${content_len}\r\n{res_data}\r\n"
+            response: str  = command_parser(command)
                 
             print(f"DEBUG: returning - {response.encode()}")
             connection.send(response.encode())

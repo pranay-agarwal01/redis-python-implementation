d34c4a34d6fd9a8ae4fe13519d8833cc4c1dd618
pranay-agarwal01 <pranay@everstage.com>
2024-10-05 18:57:53 +0530

    refactor

--- app/main.py
+++ app/main.py
@@ -2,6 +2,16 @@ import socket
 import threading
 import time
 
+class ResponseParser:
+    def respSimpleString(message):
+        return f"+{message}\r\n"
+    
+    def respBulkString(message):
+        if message is None:
+            return "$-1\r\n"
+        return f"${len(message)}\r\n{message}\r\n"
+    
+pong = "+PONG\r\n"
 db_data = {}
 
 def command_parser(command: str) -> str:
@@ -14,27 +24,27 @@ def command_parser(command: str) -> str:
     # *2\r\n$3\r\nGET\r\n$3\r\nhey\r\n
     print("DEBUG: Command Tokens", all_tokens)
     if all_tokens[0] == "*1" and all_tokens[1] == "$4" and all_tokens[2] == "PING":
-        return "+PONG\r\n"
+        return ResponseParser.respBulkString(pong)
     elif all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "ECHO":
-        return f"${len(all_tokens[4])}\r\n{all_tokens[4]}\r\n"
+        return ResponseParser.respBulkString(all_tokens[4])
     elif (all_tokens[0] == "*3" or all_tokens[0] == "*5") and all_tokens[1] == "$3" and all_tokens[2] == "SET":
         # creating a list [value, expiry]
         expiry = None
         if len(all_tokens) > 8 and all_tokens[8] == "px":
             expiry = int(time.time() * 1000) + int(all_tokens[10])
         db_data[all_tokens[4]] = [all_tokens[6], expiry]
-        return "+OK\r\n"
+        return ResponseParser.respSimpleString("OK")
     elif all_tokens[0] == "*2" and all_tokens[1] == "$3" and all_tokens[2] == "GET":
         key = all_tokens[4]
         if key not in db_data:
-            return "$-1\r\n"
+            return ResponseParser.respBulkString(None)
         value_list = db_data[key]
         is_expired = value_list[1] and value_list[1] < int(time.time() * 1000)
         if is_expired:
-            return "$-1\r\n"
-        return f"${len(value_list[0])}\r\n{value_list[0]}\r\n"
+            return ResponseParser.respBulkString(None)
+        return ResponseParser.respBulkString(value_list[0])
     else:
-        return "$-1\r\n"
+        return ResponseParser.respBulkString(None)
 
 def connect(connection: socket.socket) -> None:
     with connection:

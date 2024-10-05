d4d6a6fce29c8e6c14ec3d2d097f2551012c44f1
pranay-agarwal01 <pranay@everstage.com>
2024-10-05 23:12:36 +0530

    dir & dbfilename CONFIG

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket   
 import threading
 import time
+import argparse
 
 class ResponseParser:
     def respSimpleString(message):
@@ -11,8 +12,14 @@ class ResponseParser:
             return "$-1\r\n"
         return f"${len(message)}\r\n{message}\r\n"
     
-pong = "+PONG\r\n"
+    def respArray(messages:list):
+        if not messages:
+            return "$-1\r\n"
+        return f"*{len(messages)}" + "\r\n" + "".join([ResponseParser.respBulkString(message) for message in messages])
+    
 db_data = {}
+dir = None
+dbfilename = None
 
 def command_parser(command: str) -> str:
     all_tokens = command.split("\r\n")
@@ -24,7 +31,7 @@ def command_parser(command: str) -> str:
     # *2\r\n$3\r\nGET\r\n$3\r\nhey\r\n
     print("DEBUG: Command Tokens", all_tokens)
     if all_tokens[0] == "*1" and all_tokens[1] == "$4" and all_tokens[2] == "PING":
-        return ResponseParser.respBulkString(pong)
+        return ResponseParser.respSimpleString("PONG")
     elif all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "ECHO":
         return ResponseParser.respBulkString(all_tokens[4])
     elif (all_tokens[0] == "*3" or all_tokens[0] == "*5") and all_tokens[1] == "$3" and all_tokens[2] == "SET":
@@ -43,6 +50,13 @@ def command_parser(command: str) -> str:
         if is_expired:
             return ResponseParser.respBulkString(None)
         return ResponseParser.respBulkString(value_list[0])
+    elif all_tokens[0] == "*3" and all_tokens[1] == "$6" and all_tokens[2] == "CONFIG":
+        if all_tokens[4] == "GET":
+            if all_tokens[6] == "dbfilename":
+                return ResponseParser.respArray(["dbfilename",dbfilename])
+            if all_tokens[6] == "dir":
+                return ResponseParser.respArray(["dir",dir])
+        return ResponseParser.respBulkString(None)
     else:
         return ResponseParser.respBulkString(None)
 
@@ -57,10 +71,22 @@ def connect(connection: socket.socket) -> None:
                 
             print(f"DEBUG: returning - {response.encode()}")
             connection.send(response.encode())
+def parse_args():
+    parser = argparse.ArgumentParser(description="Redis Server")
+    parser.add_argument('--dir', type=str, help='Directory to store data in')
+    parser.add_argument('--dbfilename', type=str, help='File to store data in')
+    return parser.parse_args()
 
 def main():
+    args = parse_args()
     print("Running redis server on port 6379")
 
+    if args.dir is not None:
+        global dir 
+        dir = args.dir
+    if args.dbfilename is not None:
+        global dbfilename
+        dbfilename = args.dbfilename
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     while True:

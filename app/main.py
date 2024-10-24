2ce7e690657ad5884a21c34df5264802ac2d0225
pranay-agarwal01 <pranay@everstage.com>
2024-10-25 00:49:56 +0530

    refactor

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import secrets  
+import secrets
 import string
 import socket
 import threading
@@ -10,7 +10,10 @@ from .rdb_parser import RDBParser
 all_replica_connection: list[socket.socket] = []
 
 def create_random_alphanumeric_string(length: int) -> str:
-    return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
+    return "".join(
+        secrets.choice(string.ascii_letters + string.digits) for _ in range(length)
+    )
+
 
 class ResponseParser:
     def respSimpleString(message):
@@ -20,7 +23,7 @@ class ResponseParser:
         if message is None:
             return "$-1\r\n"
         return f"${len(message)}\r\n{message}\r\n"
-    
+
     def respRDBContent(message: bytes):
         return f"${len(message)}\r\n".encode() + message
 
@@ -53,6 +56,12 @@ class RedisServer:
         for conn in all_replica_connection:
             conn.send(command.encode())
 
+    def send_data(self, message, connection: socket.socket = None):
+        if isinstance(message, str):
+            message = message.encode()
+        print("DEBUG[returning]:", message)
+        return connection.send(message)
+
     def open_rdb_file(self):
         rdb_file_path = "./app/dump.rdb"
         if os.path.exists(rdb_file_path):
@@ -60,14 +69,13 @@ class RedisServer:
                 return f.read()
         else:
             print("RDB file not found, returning hardcoded rdb content")
-            return b'REDIS0011\xfa\tredis-ver\x057.2.0\xfa\nredis-bits\xc0@\xfa\x05ctime\xc2m\x08\xbce\xfa\x08used-mem\xc2\xb0\xc4\x10\x00\xfa\x08aof-base\xc0\x00\xff\xf0n;\xfe\xc0\xffZ\xa2'
-
+            return b"REDIS0011\xfa\tredis-ver\x057.2.0\xfa\nredis-bits\xc0@\xfa\x05ctime\xc2m\x08\xbce\xfa\x08used-mem\xc2\xb0\xc4\x10\x00\xfa\x08aof-base\xc0\x00\xff\xf0n;\xfe\xc0\xffZ\xa2"
 
     def store_key_value(self, key, value, expiry_time=None):
         # creating {key: [value, expiry]}
         self.db_data[key] = [value, expiry_time]
 
-    def command_parser(self, command: str) -> str:
+    def command_parser(self, command: str, connection: socket.socket) -> str:
         all_tokens = command.split("\r\n")
         # Sample Commands
         # *1\r\n$4\r\nPING\r\n
@@ -76,21 +84,21 @@ class RedisServer:
         # *5\r\n$3\r\nSET\r\n$9\r\npineapple\r\n$4\r\npear\r\n$2\r\npx\r\n$3\r\n100\r\n
         # *2\r\n$3\r\nGET\r\n$3\r\nhey\r\n
         # print("DEBUG: Command Tokens", all_tokens)
-        
+
         if all_tokens[0] == "*1" and all_tokens[1] == "$4" and all_tokens[2] == "PING":
-            return ResponseParser.respSimpleString("PONG")
-        
+            self.send_data(ResponseParser.respSimpleString("PONG"), connection)
+
         elif (
             all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "ECHO"
         ):
-            return ResponseParser.respBulkString(all_tokens[4])
-        
+            self.send_data(ResponseParser.respBulkString(all_tokens[4]), connection)
+
         elif (
             all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "KEYS"
         ):
             keys = list(self.db_data.keys())
-            return ResponseParser.respArray(keys)
-        
+            self.send_data(ResponseParser.respArray(keys), connection)
+
         elif (
             (all_tokens[0] == "*3" or all_tokens[0] == "*5")
             and all_tokens[1] == "$3"
@@ -101,18 +109,19 @@ class RedisServer:
                 expiry = int(time.time() * 1000) + int(all_tokens[10])
             self.store_key_value(all_tokens[4], all_tokens[6], expiry)
             self.replica_propogation_for_write_commands(command)
-            return ResponseParser.respSimpleString("OK")
-        
+            self.send_data(ResponseParser.respSimpleString("OK"), connection)
+
         elif all_tokens[0] == "*2" and all_tokens[1] == "$3" and all_tokens[2] == "GET":
             key = all_tokens[4]
             if key not in self.db_data:
-                return ResponseParser.respBulkString(None)
+                self.send_data(ResponseParser.respBulkString(None), connection)
             value_list = self.db_data[key]
             is_expired = value_list[1] and value_list[1] < int(time.time() * 1000)
             if is_expired:
-                return ResponseParser.respBulkString(None)
-            return ResponseParser.respBulkString(value_list[0])
-        
+                self.send_data(ResponseParser.respBulkString(None), connection)
+            else:
+                self.send_data(ResponseParser.respBulkString(value_list[0]), connection)
+
         elif (
             all_tokens[0] == "*3"
             and all_tokens[1] == "$6"
@@ -120,33 +129,59 @@ class RedisServer:
         ):
             if all_tokens[4] == "GET":
                 if all_tokens[6] == "dbfilename":
-                    return ResponseParser.respArray(["dbfilename", self.dbfilename])
+                    self.send_data(
+                        ResponseParser.respArray(["dbfilename", self.dbfilename]),
+                        connection,
+                    )
                 if all_tokens[6] == "dir":
-                    return ResponseParser.respArray(["dir", self.dir])
-            return ResponseParser.respBulkString(None)
+                    self.send_data(
+                        ResponseParser.respArray(["dir", self.dir]), connection
+                    )
+            else:
+                self.send_data(ResponseParser.respBulkString(None), connection)
 
-        elif all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "INFO":
+        elif (
+            all_tokens[0] == "*2" and all_tokens[1] == "$4" and all_tokens[2] == "INFO"
+        ):
             if self.replicaof is not None:
-                return ResponseParser.respBulkString(f"role:{self.role}")
-            return ResponseParser.respBulkString(f"role:{self.role}:master_replid:{self.master_replid}:master_repl_offset:{self.master_repl_offset}")
-        
-        elif all_tokens[0] == "*3" and all_tokens[1] == "$8" and all_tokens[2] == "REPLCONF":
-            return ResponseParser.respSimpleString("OK")
-        
-        elif all_tokens[0] == "*3" and all_tokens[1] == "$5" and all_tokens[2] == "PSYNC":
+                self.send_data(
+                    ResponseParser.respBulkString(f"role:{self.role}"), connection
+                )
+            else:
+                self.send_data(
+                    ResponseParser.respBulkString(
+                        f"role:{self.role}:master_replid:{self.master_replid}:master_repl_offset:{self.master_repl_offset}"
+                    ),
+                    connection,
+                )
+
+        elif (
+            all_tokens[0] == "*3"
+            and all_tokens[1] == "$8"
+            and all_tokens[2] == "REPLCONF"
+        ):
+            self.send_data(ResponseParser.respSimpleString("OK"), connection)
+
+        elif (
+            all_tokens[0] == "*3" and all_tokens[1] == "$5" and all_tokens[2] == "PSYNC"
+        ):
             master_id = all_tokens[4]
             replication_offset = all_tokens[6]
             if master_id == "?" and replication_offset == "-1":
-                response = [ResponseParser.respSimpleString(f"FULLRESYNC {self.master_replid} {self.master_repl_offset}")]
+                self.send_data(
+                    ResponseParser.respSimpleString(
+                        f"FULLRESYNC {self.master_replid} {self.master_repl_offset}"
+                    ),
+                    connection,
+                )
                 rdb_content = self.open_rdb_file()
-                rdb_response = ResponseParser.respRDBContent(rdb_content)
-                response.append(rdb_response)
+                self.send_data(ResponseParser.respRDBContent(rdb_content), connection)
                 self.add_connection_master_replication_list(self.connection)
-                return response 
-            return ResponseParser.respBulkString()
-        
+            else:
+                self.send_data(ResponseParser.respBulkString(None), connection)
+
         else:
-            return ResponseParser.respBulkString(None)
+            self.send_data(ResponseParser.respBulkString(None), connection)
 
     def connect(self, connection: socket.socket) -> None:
         with connection:
@@ -156,27 +191,21 @@ class RedisServer:
                 command: str = connection.recv(1024).decode()
                 print(f"DEBUG: recieved - {command.encode()}")
                 connected = bool(command)
-                response: str | list = self.command_parser(command)
-
-                if isinstance(response, list):
-                    for res in response:
-                        if isinstance(res, bytes):
-                            print(f"DEBUG[bytes]: returning - {res}")
-                            connection.send(res)
-                        else:
-                            print(f"DEBUG[str]: returning - {res.encode()}")
-                            connection.send(res.encode())
-                else:
-                    print(f"DEBUG[str]: returning - {response.encode()}")
-                    connection.send(response.encode())
+                self.command_parser(command, connection)
 
     def perform_handshake(self, host, port, port_to_listen_on):
         master_socket = socket.create_connection((host, port))
         master_socket.send(ResponseParser.respArray(["PING"]).encode())
         master_socket.recv(1024)
-        master_socket.send(ResponseParser.respArray(["REPLCONF", "listening-port", str(port_to_listen_on)]).encode())
+        master_socket.send(
+            ResponseParser.respArray(
+                ["REPLCONF", "listening-port", str(port_to_listen_on)]
+            ).encode()
+        )
         master_socket.recv(1024)
-        master_socket.send(ResponseParser.respArray(["REPLCONF", "capa", "psync2"]).encode())
+        master_socket.send(
+            ResponseParser.respArray(["REPLCONF", "capa", "psync2"]).encode()
+        )
         master_socket.recv(1024)
         master_socket.send(ResponseParser.respArray(["PSYNC", "?", "-1"]).encode())
         master_socket.recv(1024)
@@ -185,14 +214,16 @@ class RedisServer:
         parser = argparse.ArgumentParser(description="Redis Server")
         parser.add_argument("--dir", type=str, help="Directory to store data in")
         parser.add_argument("--dbfilename", type=str, help="File to store data in")
-        parser.add_argument("--port", type=int, default=6379, help="Port to run server on")
+        parser.add_argument(
+            "--port", type=int, default=6379, help="Port to run server on"
+        )
         parser.add_argument("--replicaof", type=str, help="Replica of host:port")
         return parser.parse_args()
 
     def run(self):
         args = self.parse_args()
         print(f"Running redis server on port {args.port}")
- 
+
         if args.dir is not None:
             self.dir = args.dir
         if args.dbfilename is not None:
@@ -226,6 +257,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# hexdump -C dump.rdb
